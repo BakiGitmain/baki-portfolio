a5759ffa39d4293623bc5a945d1c8637d9fbed69
BakiGitmain <abdullahzari2005@gmail.com>
2026-08-08 21:05:06 +0300

    Proxy backend through frontend

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,25 @@ import type {
   NextConfig,
 } from "next";
 
-const nextConfig:
-  NextConfig = {
-    allowedDevOrigins: [
-      "192.168.1.4",
-    ],
+const backendUrl =
+  "https://baki-portfolio-backend.vercel.app";
 
-    images: {
-      remotePatterns: [
-        {
-          protocol:
-            "https",
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "192.168.1.4",
+  ],
 
-          hostname:
-            "res.cloudinary.com",
-        },
-      ],
-    },
-  };
+  async rewrites() {
+    return [
+      {
+        source:
+          "/backend/:path*",
+
+        destination:
+          `${backendUrl}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
